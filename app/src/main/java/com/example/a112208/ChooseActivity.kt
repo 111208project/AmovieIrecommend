a2fbd6c8d61ffb861fa1b632a45e02df09822614
馬振閔 <jefferyma@mazhenmindeMacBook-Pro.local>
2023-07-01 14:36:55 +0800

    07/01

--- app/src/main/java/com/example/a112208/ChooseActivity.kt
+++ app/src/main/java/com/example/a112208/ChooseActivity.kt
@@ -1,11 +1,19 @@
 package com.example.a112208
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class ChooseActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_choose)
+        var btnChoose = findViewById<Button>(R.id.btnChoose)
+
+        btnChoose.setOnClickListener{
+            var intent= Intent (this, MsinActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
--- app/src/main/java/com/example/a112208/LoginActivity.kt
+++ app/src/main/java/com/example/a112208/LoginActivity.kt
@@ -8,11 +8,11 @@ import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 
-class MainActivity : AppCompatActivity() {
+class LoginActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_login)
 
         val btnLogin = findViewById<Button>(R.id.btnlogin)
         val etUsername = findViewById<EditText>(R.id.txtusername)
@@ -25,11 +25,11 @@ class MainActivity : AppCompatActivity() {
             // 这里您需要根据您的需求替换成真实的用户名和密码
             if (username == "user" && password == "123456") {
                 Toast.makeText(this, "登入成功!", Toast.LENGTH_SHORT).show()
-                val intent = Intent(this@MainActivity, ChooseActivity::class.java)
+                val intent = Intent(this@LoginActivity, ChooseActivity::class.java)
                 startActivity(intent)
             } else if(username == "admin" && password == "123456") {
                 Toast.makeText(this, "登入成功!", Toast.LENGTH_SHORT).show()
-                val intent = Intent(this@MainActivity, ChooseActivity::class.java)
+                val intent = Intent(this@LoginActivity, ChooseActivity::class.java)
                 startActivity(intent)
             }else{
                 Toast.makeText(this, "請確認使用者名稱或密碼", Toast.LENGTH_SHORT).show()
