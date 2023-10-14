54cbd507163e77420481087330cd21dfdb90836a
馬振閔 <jefferyma@mazhenmindeMacBook-Pro.local>
2023-10-14 14:06:03 +0800

    1014

--- app/src/main/java/com/example/a112208/api/ApiService.kt
+++ app/src/main/java/com/example/a112208/api/ApiService.kt
@@ -6,10 +6,15 @@ import retrofit2.http.Body
 import retrofit2.http.POST
 
 data class LoginRequest(val username: String, val password: String)
+data class InterestRequest(val interests: List<String>)
+data class RecommendedContentResponse(val recommendedContent: List<String>)
+
 
 interface ApiService {
     @POST("login")
     fun login(@Body request: LoginRequest): Call<ResponseBody>
     @POST("register") // 这里的 "register" 是你的 Flask API 提供的注册用户的端点
     fun registerUser(@Body request: LoginRequest): Call<Void>
+    @POST("send-interests") // 新增的 API 方法，用於發送興趣選擇
+    fun sendInterests(@Body request: InterestRequest): Call<RecommendedContentResponse>
 }
