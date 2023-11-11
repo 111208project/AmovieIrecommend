58434a33a13697bce39d65992ebcca87db4e7939
51xxx <11136033@ntub.edu.tw>
2023-11-11 18:24:03 +0800

    1111

--- app/src/main/java/com/example/a112208/MainActivity.kt
+++ app/src/main/java/com/example/a112208/MainActivity.kt
@@ -6,7 +6,9 @@ import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import com.example.a112208.R
+import com.example.a112208.fragments.Fragment_favorite
 import com.example.a112208.fragments.Fragment_home
+import com.example.a112208.fragments.Fragment_user
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
@@ -23,7 +25,17 @@ class MainActivity : AppCompatActivity() {
                     )
                     return@OnNavigationItemSelectedListener true
                 }
-                // 這裡可以加入其他的 Fragment
+                R.id.navigation_favorite -> {
+                    // 处理最爱的 Fragment
+                    // 例如： loadFragment(FavoriteFragment.newInstance())
+                    Toast.makeText(this, "Favorite clicked", Toast.LENGTH_SHORT).show()
+                    return@OnNavigationItemSelectedListener true
+                }
+                R.id.navigation_profile -> {
+                    loadFragment(Fragment_user.newInstance())
+                    Toast.makeText(this, "Profile clicked", Toast.LENGTH_SHORT).show()
+                    return@OnNavigationItemSelectedListener true
+                }
                 else -> return@OnNavigationItemSelectedListener false
             }
         }
