5e343003dbb95a3ff6ea1a7fc98c6843e2010760
51xxx <11136033@ntub.edu.tw>
2023-11-21 18:50:02 +0800

    1121

--- app/src/main/java/com/example/a112208/ScoreActivity.kt
+++ app/src/main/java/com/example/a112208/ScoreActivity.kt
@@ -1,70 +1,65 @@
 package com.example.a112208
 
-// MainActivity.kt
-
-// 引入必要的庫
 import android.annotation.SuppressLint
+import android.content.Context
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.ViewGroup
 import android.widget.ArrayAdapter
 import android.widget.Button
 import android.widget.EditText
 import android.widget.ListView
+import android.widget.TextView
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 
-// MainActivity 類別繼承 AppCompatActivity
 class ScoreActivity : AppCompatActivity() {
 
-    // 儲存留言的列表
     private val messages = mutableListOf<String>()
+    private lateinit var adapter: MessageAdapter
 
-    // 延遲初始化 ArrayAdapter
-    private lateinit var adapter: ArrayAdapter<String>
-
-    // 覆寫 onCreate 方法
-    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        // 設置活動的布局
         setContentView(R.layout.activity_score)
 
-        // 初始化 ArrayAdapter
-        adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, messages)
+        adapter = MessageAdapter(this, android.R.layout.simple_list_item_1, messages)
 
-        // 找到 ListView 並設置適配器
         val listView: ListView = findViewById(R.id.listViewMessages)
         listView.adapter = adapter
 
-        // 找到 EditText 和 Button
         val editTextMessage: EditText = findViewById(R.id.editTextMessage)
         val buttonSubmit: Button = findViewById(R.id.buttonSubmit)
-        // 返回按鈕
         val btnreturn = findViewById<Button>(R.id.btnReturn)
         btnreturn.setOnClickListener {
             val intent = Intent(this@ScoreActivity, MovieinfoActivity::class.java)
             startActivity(intent)
         }
-        // 設置 Button 的點擊監聽器
+
         buttonSubmit.setOnClickListener {
-            // 獲取 EditText 中的留言
             val message = editTextMessage.text.toString()
 
-            // 檢查留言是否不為空白
             if (message.isNotBlank()) {
-                // 添加留言到列表中
                 messages.add(message)
-
-                // 通知適配器數據已更改
                 adapter.notifyDataSetChanged()
-
-                // 清空 EditText
                 editTextMessage.text.clear()
             } else {
-                // 若留言為空白，顯示提示
                 Toast.makeText(this, "請輸入留言", Toast.LENGTH_SHORT).show()
             }
+        }
+    }
+
+    private inner class MessageAdapter(
+        context: Context,
+        resource: Int,
+        objects: List<String>
+    ) : ArrayAdapter<String>(context, resource, objects) {
+
+        override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+            val view = super.getView(position, convertView, parent)
+            val textView = view.findViewById<TextView>(android.R.id.text1)
+            textView.textSize = 18f // 設置字體大小，這裡以 18sp 為例
+            return view
         }
     }
 }
